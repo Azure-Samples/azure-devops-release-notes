4fc7334db3b563cb9b899aa8649134608c7143bb
Jasmine G <paladique@users.noreply.github.com>
2019-03-26 16:14:24 -0400

    update sample name

--- AzureDevOpsReleaseNotes/ReleaseNotesWebHook.cs
+++ AzureDevOpsReleaseNotes/ReleaseNotesWebHook.cs
@@ -14,7 +14,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace ADOReleaseNotes
+namespace AzureDevOpsReleaseNotes
 {
     public static class ReleaseNotesWebHook
     {
