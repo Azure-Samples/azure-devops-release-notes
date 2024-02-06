cb9b9710af60fb1e6ad89dc4fed5d340eca2b7f3
Jasmine <paladique@users.noreply.github.com>
2024-02-06 16:38:48 -0500

    include gen ai in note creation

--- AzureDevOpsReleaseNotes/ReleaseNotesWebHook.cs
+++ AzureDevOpsReleaseNotes/ReleaseNotesWebHook.cs
@@ -1,4 +1,9 @@
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -7,19 +12,21 @@ using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
-using Microsoft.WindowsAzure.Storage;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AzureDevOpsReleaseNotes
 {
     public static class ReleaseNotesWebHook
     {
         [FunctionName("ReleaseNotesWebHook")]
-        public static async void RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
             //Extract data from request body
             string requestBody = new StreamReader(req.Body).ReadToEnd();
@@ -27,8 +34,8 @@ namespace AzureDevOpsReleaseNotes
             string releaseName = data?.resource?.release?.name;
             string releaseBody = data?.resource?.release?.description;
 
-            VssBasicCredential credentials = new VssBasicCredential(Environment.GetEnvironmentVariable("DevOps.Username"), Environment.GetEnvironmentVariable("DevOps.AccessToken"));
-            VssConnection connection = new VssConnection(new Uri(Environment.GetEnvironmentVariable("DevOps.OrganizationURL")), credentials);
+            VssBasicCredential credentials = new(Environment.GetEnvironmentVariable("DevOps.Username"), Environment.GetEnvironmentVariable("DevOps.AccessToken"));
+            VssConnection connection = new(new Uri(Environment.GetEnvironmentVariable("DevOps.OrganizationURL")), credentials);
 
             //Time span of 14 days from today
             var dateSinceLastRelease = DateTime.Today.Subtract(new TimeSpan(14, 0, 0, 0));
@@ -36,32 +43,43 @@ namespace AzureDevOpsReleaseNotes
             //Accumulate closed work items from the past 14 days in text format
             var workItems = GetClosedItems(connection, dateSinceLastRelease);
             var pulls = GetMergedPRs(connection, dateSinceLastRelease);
-            
+
             //Create a new blob markdown file
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("StorageAccountConnectionString"));
-            var blobClient = storageAccount.CreateCloudBlobClient();
-            var container = blobClient.GetContainerReference("releases");
-            var blob = container.GetBlockBlobReference(releaseName + ".md");
+            BlobContainerClient container = new(Environment.GetEnvironmentVariable("StorageAccountConnectionString"), "releases");            
+            container.CreateIfNotExists();
+
+             var description = await GenerateReleaseDescriptionAsync(pulls, workItems);
 
             //Format text content of blob
-            var text = String.Format("# {0} \n {1} \n\n" + "# Work Items Resolved:" + workItems + "\n\n# Changes Merged:" + pulls, releaseName, releaseBody);
+            var text = string.Format("# {0} \n {1} \n\n" + "# Work Items Resolved:" + workItems + "\n\n# Changes Merged:" + pulls, releaseName, description.Choices[0].Message);
 
-            //Add text to blob
-            await blob.UploadTextAsync(text);
+            var blob = container.GetAppendBlobClient(releaseName + ".md");
+            blob.CreateIfNotExists();
+
+            var stream = new MemoryStream();
+            stream.Write(System.Text.Encoding.UTF8.GetBytes(text));
+
+            //Append text to blob
+            stream.Position = 0;
+           await blob.AppendBlockAsync(stream);
+
+            return new OkObjectResult("Release Notes Updated");
+    
         }
 
         public static string GetClosedItems(VssConnection connection, DateTime releaseSpan)
         {
             string project = Environment.GetEnvironmentVariable("DevOps.ProjectName");
             var workItemTrackingHttpClient = connection.GetClient<WorkItemTrackingHttpClient>();
-            
+
             //Query that grabs all of the Work Items marked "Done" in the last 14 days
-            Wiql wiql = new Wiql()
+            Wiql wiql = new()
             {
                 Query = "Select [State], [Title] " +
                         "From WorkItems Where " +
                         "[System.TeamProject] = '" + project + "' " +
-                        "And [System.State] = 'Done' " +
+                        "And [System.State] = 'Resolved' " +
+                        "OR [System.State] = 'Closed' " +
                         "And [Closed Date] >= '" + releaseSpan.ToString() + "' " +
                         "Order By [State] Asc, [Changed Date] Desc"
             };
@@ -72,13 +90,13 @@ namespace AzureDevOpsReleaseNotes
 
                 if (workItemQueryResult.WorkItems.Count() != 0)
                 {
-                    List<int> list = new List<int>();
+                    List<int> list = new();
                     foreach (var item in workItemQueryResult.WorkItems)
                     {
                         list.Add(item.Id);
                     }
 
-                    //Extraxt desired work item fields
+                    //Extract desired work item fields
                     string[] fields = { "System.Id", "System.Title" };
                     var workItems = workItemTrackingHttpClient.GetWorkItemsAsync(list, fields, workItemQueryResult.AsOf).Result;
 
@@ -86,7 +104,7 @@ namespace AzureDevOpsReleaseNotes
                     string txtWorkItems = string.Empty;
                     foreach (var workItem in workItems)
                     {
-                        txtWorkItems += String.Format("\n 1. #{0}-{1}", workItem.Id, workItem.Fields["System.Title"]);
+                        txtWorkItems += string.Format("\n 1. #{0}-{1}", workItem.Id, workItem.Fields["System.Title"]);
                     }
                     return txtWorkItems;
                 }
@@ -102,15 +120,15 @@ namespace AzureDevOpsReleaseNotes
             using (gitClient)
             {
                 //Get first repo in project
-                var releaseRepo = gitClient.GetRepositoriesAsync().Result[0]; 
+                var releaseRepo = gitClient.GetRepositoriesAsync().Result[0];
 
                 //Grabs all completed PRs merged into master branch
                 List<GitPullRequest> prs = gitClient.GetPullRequestsAsync(
                    releaseRepo.Id,
                    new GitPullRequestSearchCriteria()
                    {
-                       TargetRefName = "refs/heads/master",
-                       Status = PullRequestStatus.Completed                       
+                       TargetRefName = "refs/heads/main",
+                       Status = PullRequestStatus.Completed
 
                    }).Result;
 
@@ -118,20 +136,47 @@ namespace AzureDevOpsReleaseNotes
                 {
                     //Query that grabs PRs merged since the specified date
                     var pulls = from p in prs
-                            where p.ClosedDate >= releaseSpan
-                            select p;
+                                where p.ClosedDate >= releaseSpan
+                                select p;
 
                     //Format PR info into text
                     var txtPRs = string.Empty;
                     foreach (var pull in pulls)
                     {
-                        txtPRs += String.Format("\n 1. #{0}-{1}", pull.PullRequestId, pull.Title);
+                        txtPRs += string.Format("\n 1. #{0}-{1}", pull.PullRequestId, pull.Title);
                     }
 
                     return txtPRs;
                 }
                 return string.Empty;
             }
+        }
+
+       public static async Task<ChatCompletions> GenerateReleaseDescriptionAsync(string pulls, string workItems)
+        {
+             OpenAIClient client = new OpenAIClient(
+                new Uri(Environment.GetEnvironmentVariable("OpenAIEndpoint")),
+                new AzureKeyCredential(Environment.GetEnvironmentVariable("OpenAIKey")));
+
+            Response<ChatCompletions> completionsResponse = await client.GetChatCompletionsAsync(
+                        new ChatCompletionsOptions()
+                        {
+                            Messages =
+                            {
+                            new ChatMessage(ChatRole.System, @"You are an AI powered software project manager who is in charge of managing release notes."),
+                            new ChatMessage(ChatRole.User, @"Here's all the resolved work items and merged pull requests since the last release. Summarize them in a few sentences and make a generic description if there aren't any." + "\n\n" + "Work Items Resolved:" + workItems + "\n\n" + "Changes Merged:" + pulls ),      
+                            },
+                            Temperature = (float)0.7,
+                            MaxTokens = 800,
+                            NucleusSamplingFactor = (float)0.95,
+                            FrequencyPenalty = 0,
+                            PresencePenalty = 0,
+                            DeploymentName=Environment.GetEnvironmentVariable("ModelDeploymentName")
+                        });
+
+            ChatCompletions completions = completionsResponse.Value;
+
+          return completions;
         }
     }
 }
